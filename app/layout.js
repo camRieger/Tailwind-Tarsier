6b548367d53e5922beec7babec377d033fd99838
FKyleCarlos <francis.kylecarlos@gmail.com>
2026-01-24 13:21:30 -0700

    Updated page styling

--- app/layout.js
+++ app/layout.js
@@ -23,10 +23,23 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="absolute w-90/200 h-1/15 m-10">
-          <Header/>
-        </div>
+        <div className="flex flex-col h-screen w-full absolute">
+          <div className="grid grid-cols-2 gap-20 bg-[#B87A00] h-3/5 pt-10 px-10 pb-10">
+            <div className="bg-slate-200 text-black rounded-t-3xl">
+              <div className="h-1/10">
+                <Header/>
+              </div>
 
+              DESCRIPTION
+            </div>
+            <div className="bg-slate-500">
+              CODE
+            </div>
+          </div>
+          <div className="grid grid-cols-2 bg-[#FFCF60] h-2/5">
+            RENDER
+          </div>
+        </div>
         {children}
       </body>
     </html>
